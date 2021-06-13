5a2f9f79da735fae28b31617e7db6a1fe4f86e7d
Elena <ellevanova@gmail.com>
2021-06-13 14:37:44 +0400

    rename objects of db with SQL naming conventions

--- createOfLevanova.sql
+++ createOfLevanova.sql
@@ -1,54 +1,54 @@
-CREATE DATABASE CustomerDB_Levanova
+CREATE DATABASE customer_db_Levanova
 go
 
-use CustomerDB_Levanova
+use customer_db_Levanova
 
 go
 
-CREATE TABLE Customer (
-    CustomerId int IDENTITY(1,1) CONSTRAINT PK_CustomerId PRIMARY KEY,
-	FirstName varchar(50),
-    LastName varchar(50) NOT NULL UNIQUE,
-    CustomerPhoneNumber varchar(15) NOT NULL,
-	CustomerEmail varchar(255),
-    TotalPurchasesAmount  Money,
+CREATE TABLE customers (
+    customer_id int IDENTITY(1,1) CONSTRAINT PK_CustomerId PRIMARY KEY,
+	first_name varchar(50),
+    last_name varchar(50) NOT NULL UNIQUE,
+    phone_number varchar(15) NOT NULL,
+	customer_email varchar(255),
+    total_purchases_amount  Money,
 )
 
 go
 
-CREATE TABLE Notes(
-	NoteId int IDENTITY(1,1) CONSTRAINT PK_NoteId PRIMARY KEY,
-	CustomerId int,
-	Note varchar(255)
+CREATE TABLE notes(
+	note_id int IDENTITY(1,1) CONSTRAINT PK_NoteId PRIMARY KEY,
+	customer_id int,
+	note_text varchar(255)
 )
 
 go
 
-CREATE TABLE AddressCustomer  (
-    AddressId int IDENTITY(1,1) CONSTRAINT PK_AddressId PRIMARY KEY,
-	CustomerId int NOT NULL,
-	AddressLine varchar(100)NOT NULL,
-    AddressLine2 varchar(100) ,
-    AddressType varchar(15) NOT NULL CHECK (AddressType IN ('Shipping', 'Billing')),
-	City varchar(50) NOT NULL,
-	PostalCode char(6) NOT NULL,
-    [State]  char(20) NOT NULL,
-	Country  varchar(255) NOT NULL CHECK ( Country IN ('United States', 'Canada'))
+CREATE TABLE address_customer  (
+    address_id int IDENTITY(1,1) CONSTRAINT PK_address_id PRIMARY KEY,
+	customer_id int NOT NULL,
+	address_line varchar(100)NOT NULL,
+    address_line2 varchar(100) ,
+    address_type varchar(15) NOT NULL CHECK (address_type IN ('Shipping', 'Billing')),
+	city varchar(50) NOT NULL,
+	postal_code char(6) NOT NULL,
+    [state]  char(20) NOT NULL,
+	country  varchar(255) NOT NULL CHECK ( country IN ('United States', 'Canada'))
 )
 
 
 go
 
-ALTER TABLE Notes
-   ADD CONSTRAINT FK_Notes_CustomerId FOREIGN KEY (CustomerId)
-       REFERENCES Customer (CustomerId)
+ALTER TABLE notes
+   ADD CONSTRAINT FK_notes_customer_id FOREIGN KEY (customer_id)
+       REFERENCES customers (customer_id)
       ON DELETE CASCADE
       ON UPDATE CASCADE
 go
 
-ALTER TABLE AddressCustomer
-   ADD CONSTRAINT FK_AddressCustomer_CustomerId FOREIGN KEY (CustomerId)
-       REFERENCES Customer (CustomerId)
+ALTER TABLE address_customer
+   ADD CONSTRAINT FK_address_customer_customer_id FOREIGN KEY (customer_id)
+       REFERENCES customers (customer_id)
       ON DELETE CASCADE
       ON UPDATE CASCADE
 
--- dataOfLevanova.sql
+++ dataOfLevanova.sql
@@ -1,41 +1,44 @@
-use CustomerDB_Levanova
+use customer_db_Levanova1
 
 GO
 
 --correct dates
 
-INSERT INTO dbo.Customer (FirstName,LastName,CustomerPhoneNumber,CustomerEmail,TotalPurchasesAmount)
+INSERT INTO dbo.customers (first_name,last_name,phone_number,customer_email,total_purchases_amount)
 VALUES ('Ivan','Ivanov','+37494360360','ivan@gmail.com',785);
 
-INSERT INTO dbo.AddressCustomer(AddressLine,AddressLine2,AddressType,PostalCode,[State],Country,City,CustomerId)
-VALUES ('Filipova 23','Lenina 23','Billing','123456','State','Canada','Ottawa',(SELECT TOP 1 dbo.Customer.CustomerId from dbo.Customer where dbo.Customer.LastName='Ivanov'))
+INSERT INTO dbo.address_customer(address_line,address_line2,address_type,postal_code,[state],country,city,customer_id)
+VALUES ('Filipova 23','Lenina 23','Billing','123456','State','Canada','Ottawa',(SELECT TOP 1 dbo.customers.customer_id from dbo.customers where dbo.customers.last_name='Ivanov'))
 
-INSERT INTO dbo.Notes(Note,CustomerId)
-VALUES ('Filipova 23Ottawa',(SELECT TOP 1 dbo.Customer.CustomerId from dbo.Customer where dbo.Customer.LastName='Ivanov'))
+INSERT INTO dbo.Notes(note_text,customer_id)
+VALUES ('Filipova 23Ottawa',(SELECT TOP 1 dbo.customers.customer_id from dbo.customers where dbo.customers.last_name='Ivanov'))
 
 
 GO 
 
 --not correct date
 
-INSERT INTO dbo.Customer (FirstName,LastName,CustomerPhoneNumber,CustomerEmail,TotalPurchasesAmount)
+INSERT INTO dbo.customers (first_name,last_name,phone_number,customer_email,total_purchases_amount)
 VALUES ('Ivan','IvIvaIvIvanovIvanovIvanovIvanovIvanovIvIvanovIvanovIvanovIvanovIvanovnovIvanovIvanovIvanovIvanov','+37494360360','ivan@gmail.com',785);
 
-INSERT INTO dbo.AddressCustomer(AddressLine,AddressLine2,AddressType,PostalCode,[State],Country,City,CustomerId)
-VALUES ('Filipova 23','Lenina 23','Billing1','123456','State','Canada1','Ottawa',(SELECT TOP 1 dbo.Customer.CustomerId from dbo.Customer where dbo.Customer.LastName='Ivanov'))
+INSERT INTO  dbo.address_customer(address_line,address_line2,address_type,postal_code,[state],country,city,customer_id)
+VALUES ('Filipova 23','Lenina 23','Billing1','123456','State','Canada1','Ottawa',
+		(SELECT TOP 1 dbo.customers.customer_id from dbo.customers where dbo.customers.last_name='Ivanov'))
 
-INSERT INTO dbo.AddressCustomer(AddressLine,AddressLine2,AddressType,PostalCode,[State],Country,City,CustomerId)
-VALUES ('Filipova 23','Lenina 23','Billing','123456','State','Canada1','Ottawa',(SELECT TOP 1 dbo.Customer.CustomerId from dbo.Customer where dbo.Customer.LastName='Ivanov'))
+INSERT INTO  dbo.address_customer(address_line,address_line2,address_type,postal_code,[state],country,city,customer_id)
+VALUES ('Filipova 23','Lenina 23','Billing','123456','State','Canada1','Ottawa',
+	(SELECT TOP 1 dbo.customers.customer_id from dbo.customers where dbo.customers.last_name='Ivanov'))
 
-INSERT INTO dbo.AddressCustomer(AddressLine,AddressLine2,AddressType,PostalCode,[State],Country,City,CustomerId)
-VALUES ('Filipova 23','Lenina 23','Billing',null,'State','Canada','Ottawa',(SELECT TOP 1 dbo.Customer.CustomerId from dbo.Customer where dbo.Customer.LastName='Ivanov'))
-
-GO
-
-SELECT * FROM Customer;
-SELECT * FROM AddressCustomer;
-SELECT * FROM Notes;
+INSERT INTO dbo.address_customer(address_line,address_line2,address_type,postal_code,[state],country,city,customer_id)
+VALUES ('Filipova 23','Lenina 23','Billing',null,'State','Canada','Ottawa',
+	(SELECT TOP 1 dbo.customers.customer_id from dbo.customers where dbo.customers.last_name='Ivanov'))
 
 GO
 
---DELETE FROM Customer WHERE dbo.Customer.LastName='Ivanov'
+SELECT * FROM customers;
+SELECT * FROM address_customer;
+SELECT * FROM notes;
+
+GO
+
+--DELETE FROM customers WHERE dbo.customers.last_name='Ivanov'
